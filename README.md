e14f255206ab5d723c3d7db84ab8eea73b43e5a2
zeroonecraft <109741846+zeroonecraft@users.noreply.github.com>
2022-08-15 18:48:58 +0700

    v1

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center" width="100%">
+    <img width="33%" src="https://media.discordapp.net/attachments/972754616418713617/973508933652209664/one.png">
+</p>
+
+<h1><p align="center" width="100%">ZeroOne Craft</p></h1>
+
+
+<p align="right" width="100%">Copyright © ZeroOne Craft 2022. All Rights Reserved.</p>
---
+++ templates/209/js/buy.js
@@ -0,0 +1,6 @@
+function Buy(){
+    alert("================ 🛒 Buy 🛒 ===============\n\nKami akan mengarahkan anda ke discord untuk proses selanjutnya, dibawah ini adalah cara yang bisa anda gunakan untuk membayar pesanan anda.\n\n========== 💳 Payment Methode 💳 =========\n\n ・ Paypal \n ・ QRIS \n ・ Saweria: https://saweria.co/zeroonecraft (+5k)\n\n========================================")
+    event.preventDefault() ;
+	    window.open("https://discord.com/channels/972754615953137674/972754617098199130");
+	    timesThru = timesThru + 1 ;
+}
